4729e2a7629feb091294e048d26138e71f651e06
Jitin <2485715+jitinnair1@users.noreply.github.com>
2026-08-12 16:24:01 +0530

    wip streamline test and harness setup

--- src/exercises/fizz_buzz/c/solution.c
+++ src/exercises/fizz_buzz/c/solution.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-const char* fizzbuzz(int n) {
-    if (n % 15 == 0) return "FizzBuzz";
-    if (n % 3 == 0) return "Fizz";
-    if (n % 5 == 0) return "Buzz";
-    static char buf[16];
-    sprintf(buf, "%d", n);
-    return buf;
+const char *fizzbuzz(int n) {
+  if (n % 15 == 0)
+    return "FizzBuzz";
+  if (n % 3 == 0)
+    return "Fizz";
+  if (n % 5 == 0)
+    return "Buzz";
+  char str[16];
+  sprintf(str, "%d", n);
+  return str;
 }
--- src/exercises/fizz_buzz/c/test.c
+++ src/exercises/fizz_buzz/c/test.c
@@ -2,26 +2,20 @@
 #include <string.h>
 
 int main() {
-    struct { int input; const char* expected; } test_cases[] = {
-        {1, "1"},
-        {2, "2"},
-        {3, "Fizz"},
-        {4, "4"},
-        {5, "Buzz"},
-        {6, "Fizz"},
-        {10, "Buzz"},
-        {15, "FizzBuzz"},
-        {30, "FizzBuzz"}
-    };
-    int num_tests = sizeof(test_cases) / sizeof(test_cases[0]);
+  int inputs[] = {1, 2, 3, 4, 5, 6, 10, 15, 30};
+  const char *expected[] = {"1",    "2",    "Fizz",     "4",       "Buzz",
+                            "Fizz", "Buzz", "FizzBuzz", "FizzBuzz"};
+  int num_tests = 9;
+  int i;
 
-    for (int i = 0; i < num_tests; i++) {
-        const char* res = fizzbuzz(test_cases[i].input);
-        if (strcmp(res, test_cases[i].expected) == 0) {
-            printf("PASS: fizzbuzz(%d) == %s\n", test_cases[i].input, test_cases[i].expected);
-        } else {
-            printf("FAIL: fizzbuzz(%d) = %s, expected %s\n", test_cases[i].input, res, test_cases[i].expected);
-        }
+  for (i = 0; i < num_tests; i++) {
+    const char *res = fizzbuzz(inputs[i]);
+    if (strcmp(res, expected[i]) == 0) {
+      printf("Test passed: fizzbuzz(%d) == %s\n", inputs[i], expected[i]);
+    } else {
+      printf("Test failed: fizzbuzz(%d) = %s, expected %s\n", inputs[i], res,
+             expected[i]);
     }
-    return 0;
+  }
+  return 0;
 }
